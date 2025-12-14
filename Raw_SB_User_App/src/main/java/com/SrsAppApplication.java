02bfb9a75d0f9bc711ac502f1bbc09d1ef8a44c0
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2025-12-14 23:47:44 +0530

    Added code for All default beans

--- Raw_SB_User_App/src/main/java/com/SrsAppApplication.java
+++ Raw_SB_User_App/src/main/java/com/SrsAppApplication.java
@@ -7,7 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SrsAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SrsAppApplication.class, args);
+		ConfigurableApplicationContext context = 
+			SpringApplication.run(SrsAppApplication.class, args);
+
+		String[] beanNames = context.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+
+		// All default beans
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
 		System.out.println("Spring-Boot Raw User Application running on port No: 8585...");
 	}
 
